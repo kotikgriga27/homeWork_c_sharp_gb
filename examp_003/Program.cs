58c72cdb826e907cb32ce10ebec5636dfa1cbaed
Daria <kotikgriga0027@gmail.com>
2022-12-24 14:54:59 +0300

    Solving the fhird problem

---
+++ examp_003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if (number % 2 == 0) {
+    Console.WriteLine("Число " + number + " является чётным");
+} else {
+    Console.WriteLine("Число " + number + " является нечётным");
+}
+
