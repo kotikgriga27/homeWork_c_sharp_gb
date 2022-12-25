153524b83ee4a776c2770f6bb8902eb7a2ee9b0c
Daria <kotikgriga0027@gmail.com>
2022-12-25 08:44:35 +0300

    Solving the fourth problem

---
+++ examp_004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int i = 1;
+
+Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+Console.WriteLine("Чётные числа от 1 до " + number);
+while (i <= number) {
+    if (i % 2 == 0) {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
+
