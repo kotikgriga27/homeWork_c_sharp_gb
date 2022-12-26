74ae71519ef50689918e6caac40e0dae86ec6fd9
Daria <kotikgriga0027@gmail.com>
2022-12-27 01:46:07 +0300

    Solving the first problem

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число: ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+int count = number.ToString().Length;
+
+if (count == 3) {
+    int result = ((number / 10) % 10);
+     Console.WriteLine(result);
+} else {
+    Console.WriteLine("Вы ввели не трёхзначное число!");
+}
