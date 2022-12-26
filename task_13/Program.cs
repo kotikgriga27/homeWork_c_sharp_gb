576b4ca01450014472f34a4ed697891ac27d4fab
Daria <kotikgriga0027@gmail.com>
2022-12-27 01:51:54 +0300

    Solving the second problem

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+int count = number.ToString().Length;
+
+if (count >= 3) {
+    int step = 1;
+    int i = count;
+    while (i > 3) {
+        step = step * 10;
+        i = i - 1;
+    }
+
+    int result = (number / step) % 10;
+    Console.WriteLine(result);
+} else {
+    Console.WriteLine("Третьей цифры нет");
+}
