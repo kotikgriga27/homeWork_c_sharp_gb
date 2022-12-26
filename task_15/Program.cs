c65a8680dcb20fdb803ac635375cfd4936304c0b
Daria <kotikgriga0027@gmail.com>
2022-12-27 01:57:51 +0300

    Solving the third problem

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число от 1 до 7: ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if (number == 6 | number == 7) {
+    Console.WriteLine("Ура! Сегодня выходной :)");
+} else if (number >= 1 && number <= 7) {
+    Console.WriteLine("Сегодня рабочий день :(");
+} else {
+    Console.WriteLine("Error! Введите правильное число");
+}
