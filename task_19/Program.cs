4db35fa90fd7401c6464473e6e739ca5f3d91550
Daria <kotikgriga0027@gmail.com>
2022-12-31 09:48:13 +0300

    Solving the first problem

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число:");
+string numberStr = Console.ReadLine();
+int len = numberStr.Length;
+
+int flag = 1;
+if (len == 5) {
+    for (int i = 0; i <= len / 2 + 1; i++) {
+        if (numberStr[i] != numberStr[len - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1) {
+        Console.WriteLine("Число " + numberStr + " является палиндромом");
+    } else {
+        Console.WriteLine("Число " + numberStr + " НЕ является палиндромом");
+    }
+
+} else {
+    Console.WriteLine("Error! Введите правильное число");
+}
