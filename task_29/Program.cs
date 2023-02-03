9785df44eec073815fab676dc23e4b116c61ed1d
Daria <kotikgriga0027@gmail.com>
2023-02-03 22:23:16 +0300

    Creating a file task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите числа: ");
+int[] array = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+
+
+
+
+
