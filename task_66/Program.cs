281ee78196b27db34fc941a8a48fc6e8520a05c9
Daria <kotikgriga0027@gmail.com>
2023-02-04 03:57:53 +0300

    Creating a file task_66

---
+++ task_66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите начальное число M: ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.Write("Введите начальное число N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+//Метод нахождения суммы натуральных элементов в промежутке от M до N
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N -> {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
