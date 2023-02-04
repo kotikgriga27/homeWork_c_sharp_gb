8b66a7a466e1f9f1a50bd51a554d178f9d95a93e
Daria <kotikgriga0027@gmail.com>
2023-02-04 04:28:04 +0300

    Creating a file task_60

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -1,28 +1,48 @@
-﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
-Console.WriteLine($"\nВведите размер массива X x Y x Z:");
-int x = InputNumbers("Введите X: ");
-int y = InputNumbers("Введите Y: ");
-int z = InputNumbers("Введите Z: ");
-Console.WriteLine($"");
-
-int[,,] array3D = new int[x, y, z];
-CreateArray(array3D);
-WriteArray(array3D);
-
-int InputNumbers(string input) {
-    Console.Write(input);
-    int output = Convert.ToInt32(Console.ReadLine());
-    return output;
+//Метод создания одномерного возрастающего массива из не повторяющихся чисел:
+int [] GetOneMeasureArray (int minValue, int length) {
+    int [] array = new int [length];
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = minValue;
+        minValue++;
+    }
+    return array;
 }
 
-void WriteArray (int[,,] array3D) {
-    for (int i = 0; i < array3D.GetLength(0); i++) {
-        for (int j = 0; j < array3D.GetLength(1); j++) {
-            Console.Write($"({i}, {j}, ");
-            for (int k = 0; k < array3D.GetLength(2); k++) {
-                Console.Write( $"{k}){array3D[i,j,k]}; ");
+//Метод создания трёхмерного массива из не повторяющихся чисел:
+int [,,] GetTripleArray(int depth, int rows, int columns) {
+    int arrayRandomIndex = 1;
+    int [] oneArray = GetOneMeasureArray(-99, 199);///Создание одномерного массива двузначных чисел:
+    int [,,] array = new int [depth, rows, columns];
+    Random randomValue = new Random();
+        for (int i = 0; i < array.GetLength(0); i++) {
+            for (int j = 0; j < array.GetLength(1); j++) {
+                for (int k = 0; k < array.GetLength(2); k++) {
+                    arrayRandomIndex = randomValue.Next(109, 198);///Положиетльные двузначные числа:
+                    if (oneArray[arrayRandomIndex] == 0) {
+                        arrayRandomIndex = randomValue.Next(10, 89);///Отрицательные двузначные числа
+                    }
+                    if (array[i, j, k] == 0) {
+                        array[i, j, k] = oneArray[arrayRandomIndex];///Значение рандомного индекса одномерного массива присваивается трёхмерному:
+                        oneArray[arrayRandomIndex] = 0;///Обнуление значения индекса в одномерном массиве:
+                        k = 0;
+                        j = 0;
+                        i = 0;
+                    }
+                }
+            }
+        }
+    return array;
+}
+
+//Метод печати массива
+void PrintArray(int [,,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            for (int k = 0; k < array.GetLength(2); k++) {
+                Console.Write("{0,6}", array[i, j, k]);
             }
             Console.WriteLine();
         }
@@ -30,30 +50,23 @@ void WriteArray (int[,,] array3D) {
     }
 }
 
-void CreateArray(int[,,] array3D) {
-    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
-    int  number;
-    for (int i = 0; i < temp.GetLength(0); i++) {
-        temp[i] = new Random().Next(10, 100);
-        number = temp[i];
-        if (i >= 1) {
-            for (int j = 0; j < i; j++) {
-                while (temp[i] == temp[j]) {
-                    temp[i] = new Random().Next(10, 100);
-                    j = 0;
-                    number = temp[i];
-                }
-                number = temp[i];
-            }
-        }
-    }
-    int count = 0; 
-    for (int x = 0; x < array3D.GetLength(0); x++) {
-        for (int y = 0; y < array3D.GetLength(1); y++) {
-            for (int z = 0; z < array3D.GetLength(2); z++) {
-                array3D[x, y, z] = temp[count];
-                count++;
+//Метод построчной печати массива с добавлением индекса элемента:
+void PrintIndexedArray (int [,,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        Console.WriteLine($"\n{i + 1} мерность массива:");
+        for (int j = 0; j < array.GetLength(1); j++) {
+            for (int k = 0; k < array.GetLength(2); k++) {
+                Console.WriteLine("{0,6}", $"Индекс |{i},{j},{k}|  =  ({array[i, j, k]})");
             }
         }
     }
 }
+
+int [,,] array = GetTripleArray(2,2,2);
+Console.WriteLine();
+
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine("Построчная печать массива с добавлением идекса элемента:");
+PrintIndexedArray(array);
